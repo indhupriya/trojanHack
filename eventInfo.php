d24e11175c2bdbcca4f715b7151bc6a0f4f384a3
prasadns14 <prasadns14@gmail.com>
2015-10-10 19:49:25 -0700

    Update eventInfo.php

--- eventInfo.php
+++ eventInfo.php
@@ -11,7 +11,8 @@ $link = "ahashash";
 
 <html>
 <head>
-	<title>Event Info</title>
+	<title>Event Info</title
+	<link rel='stylesheet' href='./styles/bootstrap.min.css' />	
 </head>
 <body>
 	<div class="container">
@@ -20,37 +21,38 @@ $link = "ahashash";
 
                         <div class="form-group">
                                 <label class="control-label col-md-3">Category:</label>
-                                <label class="control-label col-md-9">$category</label>
+                                <label class="control-label col-md-9"><?php echo "$category" ?></label>
                         </div>
                         <div class="form-group">
                                 <label class="control-label col-md-3">Event Type:</label>
-                                <label class="control-label col-md-9">$eventType</label>
+                                <label class="control-label col-md-9"><?php echo "$eventType" ?></label>
                         </div>
 
                         <div class="form-group">
                                 <label class="control-label col-md-3">Location:</label>
-                                <label class="control-label col-md-9">$location</label>
+                                <label class="control-label col-md-9"><?php echo "$location" ?></label>
                         </div>
                         <div class="form-group">
                                 <label class="control-label col-md-3">Description:</label>
-                                <label class="control-label col-md-9">$desc</label>
+                                <label class="control-label col-md-9"><?php echo "$desc" ?></label>
                         </div>
 						 <div class="form-group">
                                 <label class="control-label col-md-3">Link:</label>
-                                <label class="control-label col-md-9">$link</label>
+                                <label class="control-label col-md-9"><?php echo "$link" ?></label>
                         </div>
                         <div class="form-group">
 								<div class="col-md-6">
                                         <button type="btn" class="btn btn-success" name="back">Back</button>
                                 </div>
                                 <div class="col-md-6">
-                                        <button type="btn" class="btn btn-success" name="FBPost">Share on Facebook</button>
+                                        <span class="glyphicon glyphicons-share-alt"><button type="btn" class="btn btn-success" name="FBPost"></button></span>
                                 </div>
-								<div class="col-md-6">
-                                        <button type="btn" class="btn btn-success" name="TwitterPost">Share on Twitter</button>
-                                </div>
+								
                         </div>
 
                 </form>
+		<script src='/scripts/jquery-2.1.4.min.js'></script>
+        <script src='/scripts/bootstrap.min.js'></script>
+				
 </body>
 </html>
