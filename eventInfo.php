2108c10124af060f0d18157ca008c59bca49a615
prasadns14 <prasadns14@gmail.com>
2015-10-11 10:00:48 -0700

    Update eventInfo.php

--- eventInfo.php
+++ eventInfo.php
@@ -5,7 +5,7 @@ $eventType = "asgads";
 $location = "assdsh";
 $desc = "asdhahh";
 $link = "ahashash";
-$eventName = "EVENT";
+
 ?>
 
 
@@ -13,14 +13,6 @@ $eventName = "EVENT";
 <head>
 	<title>Event Info</title
 	<link rel='stylesheet' href='./styles/bootstrap.min.css' />	
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>    
 </head>
 <body>
 	<div class="container">
@@ -29,54 +21,46 @@ $eventName = "EVENT";
 
                         <div class="form-group">
                                 <label class="control-label col-md-3">Category:</label>
-                                <label class="control-label col-md-9"><?php echo "$category" ?></label>
+                                <label class="control-label col-md-9">$category</label>
                         </div>
                         <div class="form-group">
                                 <label class="control-label col-md-3">Event Type:</label>
-                                <label class="control-label col-md-9"><?php echo "$eventType" ?></label>
-                        </div>
-                        
-                        <div class="form-group">
-                                <label class="control-label col-md-3">Event Name:</label>
-                                <label class="control-label col-md-9"><?php echo "$eventName" ?></label>
+                                <label class="control-label col-md-9">$eventType</label>
                         </div>
 
                         <div class="form-group">
                                 <label class="control-label col-md-3">Location:</label>
-                                <label class="control-label col-md-9"><?php echo "$location" ?></label>
+                                <label class="control-label col-md-9">$location</label>
                         </div>
                         <div class="form-group">
                                 <label class="control-label col-md-3">Description:</label>
-                                <label class="control-label col-md-9"><?php echo "$desc" ?></label>
+                                <label class="control-label col-md-9">$desc</label>
                         </div>
 						 <div class="form-group">
                                 <label class="control-label col-md-3">Link:</label>
-                                <label class="control-label col-md-9"><?php echo "$link" ?></label>
+                                <label class="control-label col-md-9">$link</label>
                         </div>
                         <div class="form-group">
 								<div class="col-md-6">
                                         <button type="btn" class="btn btn-success" name="back">Back</button>
                                 </div>
                                 <div class="col-md-6">
-                                        <span class="glyphicon glyphicons-share-alt"><button type="btn" class="btn btn-success" name="FBPost"></button></span>
+                                        <button type="btn" class="btn btn-success" name="FBPost"><span class="glyphicon glyphicons-share-alt" style="vertical-align:middle"></span></button>
                                 </div>
 								
                         </div>
-                    
-                        <div class="form-group">
-				<div class="col-md-12">
-					<textarea rows="10" cols="50"></textarea>
-				</div>
-			</div>
-						
-			<div class="form-group">
-				<div class="col-md-6">
+						<div class="form-group">
+								<div class="col-md-9">
+										<textarea></textarea>
+								</div>
+						</div>
+						 <div class="form-group">
+								<div class="col-md-6">
                                         <button type="btn" class="btn btn-success" name="back">Submit</button>
                                 </div>
                          
 								
                         </div>
-
                 </form>
 		<script src='/scripts/jquery-2.1.4.min.js'></script>
         <script src='/scripts/bootstrap.min.js'></script>
